d955b80e98d0ff08990a1f6a2a296a35a1584e70
Harjeet Singh <Harjeet.Singh-extern@deutschebahn.com>
2019-11-08 14:56:42 +0100

    planspiel  website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavbarComponent } from './entities/top-navbar/top-navbar.component';
+import { WelcomeComponent } from './entities/welcome/welcome.component';
+import { TeamComponent } from './entities/team/team.component';
+import { FooterComponent } from './entities/footer/footer.component';
+import { VisionComponent } from './entities/vision/vision.component';
+import { MissionComponent } from './entities/mission/mission.component';
+import { PurposeComponent } from './entities/purpose/purpose.component';
+import { ValueComponent } from './entities/value/value.component';
+import { SocialComponent } from './entities/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavbarComponent,
+    WelcomeComponent,
+    TeamComponent,
+    FooterComponent,
+    VisionComponent,
+    MissionComponent,
+    PurposeComponent,
+    ValueComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
